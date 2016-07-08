6540b44225c19a4d6f5b3a8887da48638636cb0f
Jackie00 <qqliuxiaofang@163.com>
2016-07-08 16:32:01 +0800

    firstcommit

---
+++ ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by appel on 16/5/19.
+//  Copyright © 2016年 appel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, CYLSex)
+{
+    CYLSexMan,
+    CYLSexWoman
+};
+
+@interface ViewController : UIViewController
+
+@property (nonatomic ,readonly, strong) NSArray *array;
+
+@property (nonatomic ,strong) NSString *string;
+
+@end
+
